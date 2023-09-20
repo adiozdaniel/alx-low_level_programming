9f6c0138640e70d71e926b308dc1e5a70ad3f305
adioz <adiozdaniel@gmail.com>
2023-09-21 01:51:08 +0300

    enforce int

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -16,7 +16,7 @@ int main(void)
    * - only one statement
    * - you are not allowed to code anything else than this line of code
    */
-  int offset = (int*)&a[2] - p; *(p + offset) = 98;    
+  *((int*)&a[2]) = 98;
   /* ...so that this prints 98\n */
   printf("a[2] = %d\n", a[2]);
   return (0);
