a2dbae7856db05a26f0854bb5127feea82cc6752
adiozdaniel <adiozdaniel@gmail.com>
2023-09-30 14:36:48 +0300

    clear

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -13,10 +13,10 @@ int main(void)
    * Remember:
    * - you are not allowed to use a
    * - you are not allowed to modify p
-   * - only one statement
+   * - only one statement: *(int *)((char *) (&*p) + 5 * sizeof(int)) = 98;
    * - you are not allowed to code anything else than this line of code
    */
-*(int *)((char *)(&*p) + 5 * sizeof(int)) = 98 ;
+  *(p + 5) = 98;
   /* ...so that this prints 98\n */
   printf("a[2] = %d\n", a[2]);
   return (0);
