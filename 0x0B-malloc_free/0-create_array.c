07483063a6714b849156ca6cb814b2a2899366ba
adiozdaniel <adiozdaniel@gmail.com>
2023-10-03 02:34:49 +0300

    failed to allocate memory

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,6 +11,10 @@ char *create_array(unsigned int size, char c)
 	char *array;
 	unsigned int i;
 
+	if (size == 0)
+		return (NULL);
+
+
 	array = (char *)malloc(size * sizeof(char));
 	if (array == NULL)
 		return (NULL);
