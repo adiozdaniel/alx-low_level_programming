47774b06a93d3ea84017a6164b168626892e23f7
adiozdaniel <adiozdaniel@gmail.com>
2023-10-02 23:12:28 +0300

    submit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,7 +11,7 @@ char *create_array(unsigned int size, char c)
 	char *array;
 	unsigned int i;
 
-	array = (char *)malloc((size + 1) * sizeof(char));
+	array = (char *)malloc(size * sizeof(char));
 	if (array == NULL)
 		return (NULL);
 
