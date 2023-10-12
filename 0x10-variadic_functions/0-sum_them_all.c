7fe2c98bf0a3940796c1ebc2385a050ea87793de
adiozdaniel <adiozdaniel@gmail.com>
2023-10-12 21:30:38 +0300

    2ndCorrection

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,15 +8,21 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	unsigned int i, sum = 0;
+	unsigned int i;
+	int sum = 0;
 	va_list args;
 
 	va_start(args, n);
 
 	if (n == 0)
+	{
 		return (0);
-	for (i = 0; i < n; i++)
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
 			sum += va_arg(args, int);
+	}
 
 	va_end(args);
 	return (sum);
