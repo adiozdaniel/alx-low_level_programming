a3996e7d766133757cf3f8ecb61c57a5fb668f79
adiozdaniel <adiozdaniel@gmail.com>
2023-12-11 14:49:30 +0300

    uploaded

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 wget -O ../101-make_me_win.sh "https://raw.githubusercontent.com/adiozdaniel/alx-low_level_programming/master/0x18-dynamic_libraries/101-make_me_win.sh"
 sed -i "s/\(usage: \$0 \)\([0-9]* [0-9]* [0-9]* [0-9]* [0-9]* bonus\)/\9 8 10 24 75 9 \2/" gm
-export LD_PRELOAD="$PWD/../101-make_me_win.sh"
