53d7b1f8e197b0589b568f6261d723751a7ea569
adiozdaniel <adiozdaniel@gmail.com>
2023-12-11 13:53:10 +0300

    try

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O ../101-make_me_win.sh "https://github.com/adiozdaniel/alx-low_level_programming/tree/master/0x18-dynamic_libraries/101-make_me_win.sh"
-export LD_PRELOAD="$PWD/../101-make_me_win.sh"
+wget -O ../101-make_me_win.sh "https://raw.githubusercontent.com/adiozdaniel/alx-low_level_programming/master/0x18-dynamic_libraries/101-make_me_win.sh"
+winning_numbers="9 8 10 24 75 9"; sed -i "s/\(usage: \$0 \)\([0-9]* [0-9]* [0-9]* [0-9]* [0-9]* bonus\)/\1$winning_numbers \2/" 101-make_me_win.sh && export LD_PRELOAD="$PWD/../101-make_me_win.sh"
