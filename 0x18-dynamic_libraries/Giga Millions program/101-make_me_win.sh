3053e832e46e383307565e140d89f180fa3fca5c
adiozdaniel <adiozdaniel@gmail.com>
2023-12-11 13:39:02 +0300

    checked

--- 0x18-dynamic_libraries/Giga Millions program/101-make_me_win.sh
+++ 0x18-dynamic_libraries/Giga Millions program/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ -q "https://github.com/adiozdaniel/alx-low_level_programming/tree/master/0x18-dynamic_libraries/101-make_me_win.sh"
-export LD_PRELOAD="/tmp/101-make_me_win.sh"
+wget -O ../101-make_me_win.sh "https://github.com/adiozdaniel/alx-low_level_programming/tree/master/0x18-dynamic_libraries/101-make_me_win.sh"
+export LD_PRELOAD="$PWD/../101-make_me_win.sh"
