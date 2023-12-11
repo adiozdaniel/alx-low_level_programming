2e7cfbbe83ab113194d9fe10f3c2d2d3e28e1ce7
adiozdaniel <adiozdaniel@gmail.com>
2023-12-11 14:13:03 +0300

    change path

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp -q "https://github.com/adiozdaniel/alx-low_level_programming/tree/master/0x18-dynamic_libraries/101-make_me_win.sh"
-export LD_PRELOAD="/tmp/101-make_me_win.sh"
+wget -P .. -q "https://github.com/adiozdaniel/alx-low_level_programming/tree/master/0x18-dynamic_libraries/101-make_me_win.sh"
+export LD_PRELOAD="../101-make_me_win.sh"
