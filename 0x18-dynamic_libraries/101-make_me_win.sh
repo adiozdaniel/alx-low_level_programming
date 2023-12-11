3a9049f4b8875305b41a9e93d6ee5330892d5a5a
adiozdaniel <adiozdaniel@gmail.com>
2023-12-11 15:25:15 +0300

    changes

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sed -i "s/\(usage: \$0 \)\([0-9]* [0-9]* [0-9]* [0-9]* [0-9]* bonus\)/\1Please make me win! \2/" gm
-export LD_PRELOAD="$PWD/../101-make_me_win.sh"
+sed -i "s/\(usage: \$0 \)\([0-9]* [0-9]* [0-9]* [0-9]* [0-9]* bonus\)/\19 8 10 24 75 9 \2/" gm
+export LD_PRELOAD="$PWD../101-make_me_win.sh"
--- 0x18-dynamic_libraries/Giga Millions program/101-make_me_win.sh
+++ 0x18-dynamic_libraries/Giga Millions program/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sed -i "s/\(usage: \$0 \)\([0-9]* [0-9]* [0-9]* [0-9]* [0-9]* bonus\)/\1Please make me win! \2/" gm
+sed -i 's/Sorry, try again!/&\n\techo "--> Please make me win!"; exit 0/g' gm
 export LD_PRELOAD="$PWD/../101-make_me_win.sh"
