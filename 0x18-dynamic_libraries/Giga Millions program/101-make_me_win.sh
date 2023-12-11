f8f44c1403715017a5e2259c4320f3dc4d835045
adiozdaniel <adiozdaniel@gmail.com>
2023-12-11 14:10:33 +0300

    rally

--- 0x18-dynamic_libraries/Giga Millions program/101-make_me_win.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-wget -P .. "https://github.com/adiozdaniel/alx-low_level_programming/tree/master/0x18-dynamic_libraries/101-make_me_win.sh"
-export LD_PRELOAD="$PWD/../101-make_me_win.sh"
