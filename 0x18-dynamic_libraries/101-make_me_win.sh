43df2177ffbd1988f1ba9e1df81f8cf23683c777
adiozdaniel <adiozdaniel@gmail.com>
2023-12-11 14:37:25 +0300

    boog

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-wget -O ../101-make_me_win.sh "https://raw.githubusercontent.com/adiozdaniel/alx-low_level_programming/master/0x18-dynamic_libraries/101-make_me_win.sh"; winning_numbers="9 8 10 24 75 9"; sed -i "s/\(usage: \$0 \)\([0-9]* [0-9]* [0-9]* [0-9]* [0-9]* bonus\)/\1$winning_numbers \2/" gm
+wget -O ../101-make_me_win.sh "https://raw.githubusercontent.com/adiozdaniel/alx-low_level_programming/master/0x18-dynamic_libraries/101-make_me_win.sh"
+sed -i "s/\(usage: \$0 \)\([0-9]* [0-9]* [0-9]* [0-9]* [0-9]* bonus\)/\9 8 10 24 75 9 \2/" gm
 export LD_PRELOAD="$PWD/../101-make_me_win.sh"
